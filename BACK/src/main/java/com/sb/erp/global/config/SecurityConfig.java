ed7d0018ae9b261650128489bf2089f460763fe3
sujeong <mdfy0308@gmail.com>
2026-09-08 15:13:46 +0900

    fix: add production domain to CORS

--- BACK/src/main/java/com/sb/erp/global/config/SecurityConfig.java
+++ BACK/src/main/java/com/sb/erp/global/config/SecurityConfig.java
@@ -125,7 +125,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
  
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));  //★ Front 포트번호
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://sberp-js.duckdns.org"));  //★ Front 포트번호
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
